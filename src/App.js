31691b57dc6618b52100a226c4692eede3d438c4
Michalis Merkouris <mimerkou@gmail.com>
2022-05-10 22:30:13 +0300

    pages and react-router set

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Watchlist from './components/Watchlist';
+import Watched from './components/Watched';
+import Add from './components/Add';
 import './App.css';
 
 const App = () => {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Watchlist />} />
+          <Route path="/watched" element={<Watched />} />
+          <Route path="/add" element={<Add />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
